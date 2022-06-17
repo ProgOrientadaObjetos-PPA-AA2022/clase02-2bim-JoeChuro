39e45e9666319341da307817248b0f7d77b6e4c5
JoeChuro <joechuro1206@gmail.com>
2022-06-17 16:52:43 -0500

    Partipación clase 17 junio 2022

---
+++ EjemploHerencia02/HerenciaUno/src/paquete8/EstudiantePresencial.java
@@ -0,0 +1,74 @@
+
+package paquete8;
+
+import paquete6.*;
+
+public class EstudiantePresencial extends Estudiante{
+    
+    int numeroCreditos;
+    double costoCredito;
+    double matriculaPresencial;
+    
+    public EstudiantePresencial(String n, String ap, String iden, int e,
+            int numAsg, double costoAsg){
+        super(ap, iden, e); // ap = apellido // iden = identificacion // e = edad
+        establecerNombresEstudiante(n);
+        establecerApellidoEstudiante(ap);
+        numeroCreditos = numAsg;
+        costoCredito = costoAsg;
+    }
+    public void establecerApellidoEstudiante(String ape){
+        apellidosEstudiante = ape.toLowerCase(); // Juan.toUpperCase() >> JUAN 
+    }
+    //  Métodos establecer y calcular para los datos o atributos de la clase
+    // 2.  Método establecerNumeroCreditos(numero: Real)
+    public void establecerNumeroCreditos(int numero){
+        numeroCreditos = numero;
+    }
+    
+    // 3.  Método establecerCostoCredito(valor: Real)
+    public void establecerCostoCredito(double valor){
+        costoCredito = valor;
+    }
+
+    // 4.  Método calcularMatriculaPresencial()
+    public void calcularMatriculaPresencial(){
+        matriculaPresencial = numeroCreditos * costoCredito;
+    }
+
+    //  Métodos obtener para los datos o atributos de la clase
+    // 5. Método obtenerNumeroCreditos() : Entero
+    public int obtenerNumeroCreditos(){
+        return numeroCreditos; 
+    }
+
+    // 6. Método obtenerCostoCredito() : Real
+    public double obtenerCostoCredito(){
+        return costoCredito;
+    }
+
+    // 7. Método obtenerMatriculaPresencial() : Real
+    public double obtenerMatriculaPresencial(){
+        return matriculaPresencial;
+    }
+    @Override
+    public String toString(){
+        
+        String cadenaFinal = String.format("Nombre: %s\n"
+                + "Apellido: %s\n"
+                + "Identificación: %s\n"
+                + "Edad: %s\n"
+                + "Costo Asignatura: %.2f\n"
+                + "Número de Asignaturas: %d\n"
+                + "Total Matricula: %.2f\n",
+                nombresEstudiante.toLowerCase(),
+                apellidosEstudiante,
+                identificacionEstudiante,
+                edadEstudiante,
+                obtenerCostoCredito(),
+                obtenerNumeroCreditos(),
+                obtenerMatriculaPresencial());
+        
+        return cadenaFinal;
+    }
+}
